d964c6d79478547d6c9b28dab0537716b35b2f66
Brian Zuber <zuber573@morrisville.edu>
2022-03-08 14:42:38 -0500

    Torque to Rotate

--- Assets/Scripts/playercontroller.cs
+++ Assets/Scripts/playercontroller.cs
@@ -9,15 +9,22 @@ public class playercontroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+       rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
-        {
+        {   
             rb2d.AddTorque(torqueAmount);
+        
         }
-    }
+    
+        else if(Input.GetKey(KeyCode.RightArrow))
+            {   
+                rb2d.AddTorque(-torqueAmount);
+            }
+      
+          }      
 }
