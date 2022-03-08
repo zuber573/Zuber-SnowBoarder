a98b923c1ab61ea98b302beb362de137a2d67a73
Brian Zuber <zuber573@morrisville.edu>
2022-03-08 15:40:30 -0500

    Introducing Particle Effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
-{
+{   
+    [SerializeField] float loadDelay = 0.5f;
      void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene" , loadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene" , loadDelay);
     }   }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
