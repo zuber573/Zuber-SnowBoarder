846f2428eef75ebffa2e9b24fb0d77d581b684b8
Brian Zuber <zuber573@morrisville.edu>
2022-03-10 14:09:43 -0500

    Triggering Particle Effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {   
     [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
      void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene" , loadDelay);
         }
     }
