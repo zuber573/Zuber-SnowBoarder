a585bc00b368f18181e6508e5ef4d39e927ef4f5
Brian Zuber <zuber573@morrisville.edu>
2022-03-10 14:38:19 -0500

    Using FindObjectOfType

--- Assets/Scripts/playercontroller.cs
+++ Assets/Scripts/playercontroller.cs
@@ -1,20 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {   
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
     void Start()
     {
        rb2d = GetComponent<Rigidbody2D>();
+       surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
-    void Update()
+     void Update() 
     {
+        RotatePlayer();
+        RespondToBoost();
+
+    }
+
+    void RotatePlayer()
+    {
+       {
         if(Input.GetKey(KeyCode.LeftArrow))
         {   
             rb2d.AddTorque(torqueAmount);
@@ -27,4 +38,19 @@ public class playercontroller : MonoBehaviour
             }
       
           }      
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
 }
